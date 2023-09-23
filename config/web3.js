bafa159d187191e88d1f504cc0cde1081545f563
John Matters <john.matters@princeton.edu>
2023-09-23 16:40:07 -0400

    remove excessive files

--- config/web3.js
+++
@@ -1,8 +0,0 @@
-const Web3 = require('web3');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_URL));
-
-module.exports = web3;
--- routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postProject } = require('../controllers/projectController');
-
-router.post('/', postProject);
-
-module.exports = router;
